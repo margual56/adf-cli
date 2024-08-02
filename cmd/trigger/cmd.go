c46efd5230428e70c9353219da37a1ec00aeda8a
Marcos Gutiérrez Alonso <marcos56@mailbox.org>
2024-08-02 11:33:41 +0200

    Add license to all files

--- cmd/trigger/cmd.go
+++ cmd/trigger/cmd.go
@@ -1,6 +1,20 @@
 /*
-Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+Copyright © 2024 Marcos Gutiérrez Alonso marcos56@mailbox.org
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
+
 package trigger
 
 import (
@@ -13,31 +27,41 @@ import (
 	"github.com/spf13/viper"
 )
 
+// Get the subscriptionId, resourceGroupName, and factoryName from the environment variables or command line flags.
+// If the values are not set, the program will exit.
 func GetArgs(cmd *cobra.Command, args []string) (string, string, string) {
 	var subscriptionId string
 	if viper.IsSet("subscriptionId") {
 		subscriptionId = viper.GetString("subscriptionId")
-	} else {
+	} else if cmd.Flag("subscriptionId").Value.String() != "" {
 		subscriptionId = cmd.Flag("subscriptionId").Value.String()
+	} else {
+		log.Fatalf("subscriptionId is required")
 	}
 
 	var resourceGroupName string
 	if viper.IsSet("resourceGroupName") {
 		resourceGroupName = viper.GetString("resourceGroupName")
-	} else {
+	} else if cmd.Flag("resourceGroupName").Value.String() != "" {
 		resourceGroupName = cmd.Flag("resourceGroupName").Value.String()
+	} else {
+		log.Fatalf("resourceGroupName is required")
 	}
 
 	var factoryName string
 	if viper.IsSet("factoryName") {
 		factoryName = viper.GetString("factoryName")
-	} else {
+	} else if cmd.Flag("factoryName").Value.String() != "" {
 		factoryName = cmd.Flag("factoryName").Value.String()
+	} else {
+		log.Fatalf("factoryName is required")
 	}
 
 	return subscriptionId, resourceGroupName, factoryName
 }
 
+// Create a new client factory with the default Azure credential
+// If the credential cannot be obtained, the program will exit.
 func GetClientFactory(subscriptionId string) (*armdatafactory.ClientFactory, error) {
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
@@ -49,21 +73,20 @@ func GetClientFactory(subscriptionId string) (*armdatafactory.ClientFactory, err
 // triggerCmd represents the trigger command
 var TriggerCmd = &cobra.Command{
 	Use:   "trigger",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "",
+	Long:  "",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("trigger called")
+		// Explain that trigger needs a subcommand and suggest the subcommands available
+		fmt.Println("trigger needs a subcommand. Available subcommands are:")
+		for _, cmd := range cmd.Commands() {
+			fmt.Printf("  %s\n", cmd.Use)
+		}
 	},
 }
 
 func init() {
-	TriggerCmd.AddCommand(GetTriggerCmd)
 	TriggerCmd.AddCommand(ListTriggerCmd)
+	TriggerCmd.AddCommand(GetTriggerCmd)
 	TriggerCmd.AddCommand(StartTriggerCmd)
 	TriggerCmd.AddCommand(StopTriggerCmd)
 
