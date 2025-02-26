5dad65e6de39e70406f2996435573d72e57e465c
Marcos Gutiérrez Alonso <30444886+margual56@users.noreply.github.com>
2025-02-26 12:35:00 +0100

    Hotfix version variable in install script

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # Note: we cannot assume we're running bash and use the set -euo pipefail approach.
 set -e
 
-version="0.2.4"
+VERSION="0.2.4"
 FILE="adf-cli_$VERSION"
 
 # Include operating system in file name.
